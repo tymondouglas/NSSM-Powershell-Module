4f3f8434320eb81bdd1184077a8e8010b3eb43ce
Tymon Jonko Douglas <TymonDouglas@outlook.com>
2021-08-28 16:40:34 -0300

    First version, lists active NSSM services installed

---
+++ NSSM-Utils.psm1
@@ -0,0 +1,8 @@
+﻿Function Get-NSSMServices{
+    $NSSMServices = Get-CimInstance -ClassName win32_service | ?{$_.PathName -match 'nssm'} | Select Name,State
+    foreach ($service in $NSSMServices){
+        $service | Add-Member -NotePropertyName Application -NotePropertyValue (nssm get $service.Name Application)
+    }
+    $NSSMServices | Select Name,Application,State
+
+}
